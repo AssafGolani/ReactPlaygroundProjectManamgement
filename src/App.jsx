76e89953c23d544ff2558e92c8ce6208c94d779d
Assaf <1214assaf@gmail.com>
2024-02-09 17:46:22 +0200

    Add Modal component and handle validation in NewProject component

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,19 @@ function App() {
   }
 
   function handleAddProject(projectData) {
+    const projectId = Math.random();
     setProjectsState((prevState) => {
       const newProject = {
         ...projectData,
-        id: Math.random(),
+        id: projectId,
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-
-  console.log(projectsState);
 
   let content;
   if (projectsState.selectedProjectId === null) {
@@ -42,7 +42,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
